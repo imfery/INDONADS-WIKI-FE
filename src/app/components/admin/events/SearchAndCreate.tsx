249c8b88ecdac81d8863b808942fcd5cde0ec475
Fery <emailferyy@gmail.com>
2024-08-20 01:41:10 +0700

    merge conflict

--- src/app/components/admin/events/SearchAndCreate.tsx
+++ src/app/components/admin/events/SearchAndCreate.tsx
@@ -1,14 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
 
-<<<<<<< HEAD
-import Button from '@/components/buttons/Button';
-=======
 import { cn } from '@/lib/utils';
 
 import { Button } from '@/components/ui/button'; // Assuming you have this component
 import { Input } from '@/components/ui/input'; // Assuming you have this component
->>>>>>> 5a912f09b7d08db78bb527c977f2dd325496593a
 
 interface SearchAndCreateProps {
   onSearch: (query: string) => void;
@@ -30,11 +26,7 @@ const SearchAndCreate: React.FC<SearchAndCreateProps> = ({
         />
       </div>
       <Link href='/admin/events/create'>
-<<<<<<< HEAD
-        <Button variant='primary' className='ml-4' onClick={onCreate}>
-=======
         <Button className='ml-4' onClick={onCreate}>
->>>>>>> 5a912f09b7d08db78bb527c977f2dd325496593a
           Create Event
         </Button>
       </Link>
