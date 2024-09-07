3a3c5ba767a39613f60f40b68ffc10c38f56356d
Fery <emailferyy@gmail.com>
2024-09-08 02:18:56 +0700

    path

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/v1/:path*',
+                source: '/v1/:path*',
                 destination: `${process.env.NEXT_PUBLIC_API_URL}/v1/:path*`,
             },
         ];
