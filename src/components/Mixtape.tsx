579e8018a3bc4a2e2127ea848a1bfabfc381a6c8
Fery <61219099+imfery@users.noreply.github.com>
2024-12-06 22:53:36 +0700

    change title

--- src/components/Mixtape.tsx
+++ src/components/Mixtape.tsx
@@ -5,7 +5,7 @@ const Mixtape: React.FC = () => (
     <Card className='bg-white dark:bg-[#18181B] shadow-md rounded-lg mb-8'>
         <CardHeader className='border-b border-gray-200 dark:border-gray-600'>
             <CardTitle className='text-xl-2 font-semibold text-gray-900 dark:text-white'>
-                Listen to Indonads Radio
+                Listen to Indonads Mixtape
             </CardTitle>
         </CardHeader>
         <CardContent className='p-4'>
