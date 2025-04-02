752b13747d64887d937f29cb33017e479f1cdd59
Fery <61219099+imfery@users.noreply.github.com>
2025-04-02 18:37:51 +0700

    updated mixtape

--- src/components/Mixtape.tsx
+++ src/components/Mixtape.tsx
@@ -14,7 +14,7 @@ const Mixtape: React.FC = () => (
                     <iframe
                         width='100%'
                         height='120'
-                        src='https://player-widget.mixcloud.com/widget/iframe/?feed=%2Fbolan999%2Fmonad-mixtape-valentine-edition%2F'
+                        src='https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Fbolan999%2Fmonad-mixtape-habibi-version%2F'
                         frameBorder='0'
                         allow='autoplay'
                         className='rounded-md'
