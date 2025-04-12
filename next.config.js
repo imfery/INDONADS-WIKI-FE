e18f2544fdef552b6758d1fa469d2e1c25f19298
Fery <61219099+imfery@users.noreply.github.com>
2025-04-12 18:56:38 +0700

    added minimumCacheTTL

--- next.config.js
+++ next.config.js
@@ -16,6 +16,8 @@ const nextConfig = {
 
     images: {
         dangerouslyAllowSVG: true,
+        minimumCacheTTL: 2678400,
+        formats: ['image/webp'],
         remotePatterns: [
             {
                 protocol: 'https',
